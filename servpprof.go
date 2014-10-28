6469716ba6b1218f8b2693dd9de2977de6a24534
Burcu Dogan <jbd@google.com>
2014-10-28 00:18:31 -0700

    Reorg and cleanup.

--- servpprof.go
+++ servpprof.go
@@ -1,51 +1,92 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
-	"github.com/rakyll/servpprof/pprof"
 	"github.com/rakyll/servpprof/pprof/internal/fetch"
+	"github.com/rakyll/servpprof/pprof/internal/profile"
 	"github.com/rakyll/servpprof/pprof/internal/report"
 	"github.com/rakyll/servpprof/pprof/internal/symbolz"
 )
 
-var listen = flag.String("listen", "localhost:6464", "the hostname and port the server is listening to")
+var (
+	listen = flag.String("listen", "localhost:6464", "the hostname and port the server is listening to")
+	dest   = flag.String("target", "http://localhost:6060", "the target process that enables pprof debug server")
+)
+
+var (
+	// TODO(jbd): Support all profiles, including custom profiles.
+	cpuRpt  = &Report{name: "profile", secs: 10}
+	heapRpt = &Report{name: "heap"}
+)
+
+type Report struct {
+	mu sync.Mutex
+	p  *profile.Profile
+
+	name string
+	secs int
+}
+
+func (r *Report) Fetch() error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	// TODO(jbd): Set timeout according to the seonds parameter.
+	url := fmt.Sprintf("%s/debug/pprof/%s?seconds=%s", *dest, r.name, r.secs)
+	p, err := fetch.FetchProfile(url, 60*time.Second)
+	if err != nil {
+		return err
+	}
+	if err := symbolz.Symbolize(fmt.Sprintf("%s/debug/pprof/symbol", *dest), fetch.PostURL, p); err != nil {
+		return err
+	}
+	r.p = p
+	return nil
+}
+
+func (r *Report) String(cum bool) string {
+	if r.p == nil {
+		return ""
+	}
+	buf := bytes.NewBuffer(nil)
+	rpt := report.NewDefault(r.p, report.Options{
+		OutputFormat:   report.Text,
+		CallTree:       true,
+		CumSort:        cum,
+		PrintAddresses: true,
+	})
+	report.Generate(buf, rpt, nil)
+	return buf.String()
+}
+
+func (r *Report) Filter(cum bool, focus string) string {
+	if r.p == nil {
+		return ""
+	}
+	c := r.p.Copy()
+	c.FilterSamplesByName(nil, nil, nil)
+	buf := bytes.NewBuffer(nil)
+	rpt := report.NewDefault(c, report.Options{
+		OutputFormat:   report.Text,
+		CallTree:       true,
+		CumSort:        cum,
+		PrintAddresses: true,
+	})
+	report.Generate(buf, rpt, nil)
+	return buf.String()
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r)
-		dest := r.FormValue("dest")
-		profile := r.FormValue("p")
-		if dest == "" {
-			dest = "http://localhost:6060"
-		}
-		if profile == "" {
-			profile = "profile"
-		}
-		url := fmt.Sprintf("%s/debug/pprof/%s?seconds=%s", dest, profile, r.FormValue("seconds"))
-		fmt.Println(url)
-		p, err := fetch.FetchProfile(url, 60*time.Second)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "error: %v", err)
-			return
-		}
-		if err := symbolz.Symbolize(fmt.Sprintf("%s/debug/pprof/symbol", dest), fetch.PostURL, p); err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "error: %v", err)
-			return
-		}
-		rpt := report.NewDefault(p, report.Options{
-			OutputFormat:   report.Text,
-			CallTree:       true,
-			CumSort:        true,
-			PrintAddresses: true,
-		})
-		report.Generate(w, rpt, &pprof.ObjTool{})
-	})
+	cpuRpt.Fetch()
+	heapRpt.Fetch()
+
+	fmt.Println(cpuRpt.String(true))
+	fmt.Println(heapRpt.String(true))
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
