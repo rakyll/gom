67df81e1162cc8291d29a27f000b56fa8d92e5fc
Burcu Dogan <jbd@google.com>
2014-10-27 22:41:06 -0700

    Cleanup.

--- servpprof.go
+++ servpprof.go
@@ -3,10 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/rakyll/servpprof/pprof"
@@ -15,16 +13,21 @@ import (
 	"github.com/rakyll/servpprof/pprof/internal/symbolz"
 )
 
-var target = flag.String("dest", "http://localhost:6060", "")
 var listen = flag.String("listen", "localhost:6464", "the hostname and port the server is listening to")
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r)
 		dest := r.FormValue("dest")
+		profile := r.FormValue("p")
 		if dest == "" {
 			dest = "http://localhost:6060"
 		}
-		url := fmt.Sprintf("%s/debug/pprof/heap?seconds=10", dest)
+		if profile == "" {
+			profile = "profile"
+		}
+		url := fmt.Sprintf("%s/debug/pprof/%s?seconds=%s", dest, profile, r.FormValue("seconds"))
+		fmt.Println(url)
 		p, err := fetch.FetchProfile(url, 60*time.Second)
 		if err != nil {
 			w.WriteHeader(500)
@@ -36,7 +39,6 @@ func main() {
 			fmt.Fprintf(w, "error: %v", err)
 			return
 		}
-
 		rpt := report.NewDefault(p, report.Options{
 			OutputFormat:   report.Text,
 			CallTree:       true,
@@ -44,30 +46,6 @@ func main() {
 			PrintAddresses: true,
 		})
 		report.Generate(w, rpt, &pprof.ObjTool{})
-	})
-
-	http.HandleFunc("/sample", func(w http.ResponseWriter, r *http.Request) {
-		profile := r.FormValue("profile")
-		sec, _ := strconv.Atoi(r.FormValue("seconds"))
-		if profile == "" {
-			profile = "profile"
-		}
-		c := &http.Client{Timeout: time.Hour}
-		url := fmt.Sprintf("%s/debug/pprof/%s?seconds=%d", *target, profile, sec)
-		resp, err := c.Get(url)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "error: %v", err)
-			return
-		}
-		defer resp.Body.Close()
-		all, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "error: %v", err)
-			return
-		}
-		fmt.Fprintf(w, string(all))
 	})
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
