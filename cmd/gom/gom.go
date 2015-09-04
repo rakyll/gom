f98e63f1c164c5499e79ec5503c02ca3483c5003
Burcu Dogan <jbd@google.com>
2015-09-04 15:37:08 -0700

    Reorg the package.

--- cmd/gom/gom.go
+++ cmd/gom/gom.go
@@ -15,25 +15,15 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-	"os/exec"
 	"regexp"
 	"strconv"
-	"sync"
-	"time"
 
-	"github.com/rakyll/gom/internal/pprof/fetch"
-	"github.com/rakyll/gom/internal/pprof/profile"
-	"github.com/rakyll/gom/internal/pprof/report"
-	"github.com/rakyll/gom/internal/pprof/symbolz"
 	"github.com/rakyll/statik/fs"
 
 	_ "github.com/rakyll/gom/statik"
@@ -44,85 +34,11 @@ var (
 	target = flag.String("target", "http://localhost:6060", "the target process that enables pprof debug server")
 )
 
-var (
-	// TODO(jbd): Support all profiles, including custom profiles.
-	reports = make(map[string]*Report)
-)
+var reports = make(map[string]*Report)
 
-type Report struct {
-	mu sync.Mutex
-	p  *profile.Profile
-
-	name string
-	secs int
-}
-
-func (r *Report) Inited() bool {
-	return r.p != nil
-}
-
-// Fetch fetches the current profile and the symbols from the target program.
-func (r *Report) Fetch(secs int) error {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-	if secs == 0 {
-		secs = r.secs
-	}
-	// TODO(jbd): Set timeout according to the seonds parameter.
-	url := fmt.Sprintf("%s/debug/pprof/%s?seconds=%d", *target, r.name, secs)
-	p, err := fetch.FetchProfile(url, 60*time.Second)
-	if err != nil {
-		return err
-	}
-	if err := symbolz.Symbolize(fmt.Sprintf("%s/debug/pprof/symbol", *target), fetch.PostURL, p); err != nil {
-		return err
-	}
-	r.p = p
-	return nil
-}
-
-// Filter filters the report with a focus regex. If no focus is provided,
-// it reports back with the entire set of calls.
-// Focus regex works on the package, type and function names. Filtered
-// results will include parent samples from the call graph.
-func (r *Report) Filter(w io.Writer, cum bool, focus *regexp.Regexp) {
-	// TODO(jbd): Support ignore and hide regex parameters.
-	if r.p == nil {
-		return
-	}
-	c := r.p.Copy()
-	c.FilterSamplesByName(focus, nil, nil)
-	rpt := report.NewDefault(c, report.Options{
-		OutputFormat:   report.JSON,
-		CumSort:        cum,
-		PrintAddresses: true,
-	})
-	report.Generate(w, rpt, nil)
-}
-
-func (r *Report) Draw(w io.Writer, cum bool, focus *regexp.Regexp) error {
-	// TODO(jbd): Support ignore and hide regex parameters.
-	if r.p == nil {
-		return errors.New("no such profile")
-	}
-	c := r.p.Copy()
-	c.FilterSamplesByName(focus, nil, nil)
-	rpt := report.NewDefault(c, report.Options{
-		OutputFormat: report.Dot,
-		CumSort:      cum,
-	})
-	data := bytes.NewBuffer(nil)
-	report.Generate(data, rpt, nil)
-	cmd := exec.Command("dot", "-Tsvg")
-	in, _ := cmd.StdinPipe()
-	_, err := io.Copy(in, data)
-	if err != nil {
-		return err
-	}
-	in.Close()
-	out, err := cmd.Output()
-	_, err = w.Write(out)
-	return err
+func init() {
+	reports["profile"] = &Report{name: "profile", secs: 30}
+	reports["heap"] = &Report{name: "heap"}
 }
 
 func main() {
@@ -130,70 +46,10 @@ func main() {
 	// stats is a proxifying target/debug/pprofstats.
 	// TODO(jbd): If the UI frontend knows about the target, we
 	// might have eliminated the proxy handler.
-	http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
-		url := fmt.Sprintf("%s/debug/pprofstats", *target)
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Print(err)
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "%v", err)
-			return
-		}
-		defer resp.Body.Close()
-		all, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "%v", err)
-			return
-		}
-		if resp.StatusCode != http.StatusOK {
-			w.WriteHeader(500)
-			fmt.Fprintf(w, "%s", all)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, "%s", all)
-	})
+	http.HandleFunc("/stats", statsHandler)
 
 	// p responds back with a profile report.
-	http.HandleFunc("/p", func(w http.ResponseWriter, r *http.Request) {
-		profile := r.FormValue("profile")
-		filter := r.FormValue("filter")
-		img, _ := strconv.ParseBool(r.FormValue("img"))
-		cumsort, _ := strconv.ParseBool(r.FormValue("cumsort"))
-		force, _ := strconv.ParseBool(r.FormValue("force"))
-
-		rpt, ok := reports[profile]
-		if !ok {
-			w.WriteHeader(404)
-			fmt.Fprintf(w, "Profile not found.")
-			return
-		}
-		if !rpt.Inited() || force {
-			if err := rpt.Fetch(0); err != nil {
-				w.WriteHeader(500)
-				fmt.Fprintf(w, "%v", err)
-				return
-			}
-		}
-		var re *regexp.Regexp
-		var err error
-		if filter != "" {
-			re, err = regexp.Compile(filter)
-		}
-		if err != nil {
-			w.WriteHeader(400)
-			fmt.Fprintf(w, "%v", err)
-			return
-		}
-		if img {
-			w.Header().Set("Content-Type", "image/svg+xml")
-			rpt.Draw(w, cumsort, re)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		rpt.Filter(w, cumsort, re)
-	})
+	http.HandleFunc("/p", profileHandler)
 
 	statikFS, err := fs.New()
 	if err != nil {
@@ -211,8 +67,66 @@ func main() {
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
 
-func init() {
-	// TODO(jbd): Support user profiles.
-	reports["profile"] = &Report{name: "profile", secs: 30}
-	reports["heap"] = &Report{name: "heap"}
+func statsHandler(w http.ResponseWriter, r *http.Request) {
+	url := fmt.Sprintf("%s/debug/pprofstats", *target)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	defer resp.Body.Close()
+	all, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "%s", all)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%s", all)
+}
+
+func profileHandler(w http.ResponseWriter, r *http.Request) {
+	profile := r.FormValue("profile")
+	filter := r.FormValue("filter")
+	img, _ := strconv.ParseBool(r.FormValue("img"))
+	cumsort, _ := strconv.ParseBool(r.FormValue("cumsort"))
+	force, _ := strconv.ParseBool(r.FormValue("force"))
+
+	rpt, ok := reports[profile]
+	if !ok {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "Profile not found.")
+		return
+	}
+	if !rpt.Inited() || force {
+		if err := rpt.Fetch(0); err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+	}
+	var re *regexp.Regexp
+	var err error
+	if filter != "" {
+		re, err = regexp.Compile(filter)
+	}
+	if err != nil {
+		w.WriteHeader(400)
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	if img {
+		w.Header().Set("Content-Type", "image/svg+xml")
+		rpt.Draw(w, cumsort, re)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	rpt.Filter(w, cumsort, re)
 }
---
+++ cmd/gom/report.go
@@ -0,0 +1,107 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"os/exec"
+	"regexp"
+	"sync"
+	"time"
+
+	"github.com/rakyll/gom/internal/pprof/fetch"
+	"github.com/rakyll/gom/internal/pprof/profile"
+	"github.com/rakyll/gom/internal/pprof/report"
+	"github.com/rakyll/gom/internal/pprof/symbolz"
+)
+
+type Report struct {
+	mu sync.Mutex
+	p  *profile.Profile
+
+	name string
+	secs int
+}
+
+func (r *Report) Inited() bool {
+	return r.p != nil
+}
+
+// Fetch fetches the current profile and the symbols from the target program.
+func (r *Report) Fetch(secs int) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if secs == 0 {
+		secs = r.secs
+	}
+	// TODO(jbd): Set timeout according to the seonds parameter.
+	url := fmt.Sprintf("%s/debug/pprof/%s?seconds=%d", *target, r.name, secs)
+	p, err := fetch.FetchProfile(url, 60*time.Second)
+	if err != nil {
+		return err
+	}
+	if err := symbolz.Symbolize(fmt.Sprintf("%s/debug/pprof/symbol", *target), fetch.PostURL, p); err != nil {
+		return err
+	}
+	r.p = p
+	return nil
+}
+
+// Filter filters the report with a focus regex. If no focus is provided,
+// it reports back with the entire set of calls.
+// Focus regex works on the package, type and function names. Filtered
+// results will include parent samples from the call graph.
+func (r *Report) Filter(w io.Writer, cum bool, focus *regexp.Regexp) {
+	// TODO(jbd): Support ignore and hide regex parameters.
+	if r.p == nil {
+		return
+	}
+	c := r.p.Copy()
+	c.FilterSamplesByName(focus, nil, nil)
+	rpt := report.NewDefault(c, report.Options{
+		OutputFormat:   report.JSON,
+		CumSort:        cum,
+		PrintAddresses: true,
+	})
+	report.Generate(w, rpt, nil)
+}
+
+func (r *Report) Draw(w io.Writer, cum bool, focus *regexp.Regexp) error {
+	// TODO(jbd): Support ignore and hide regex parameters.
+	if r.p == nil {
+		return errors.New("no such profile")
+	}
+	c := r.p.Copy()
+	c.FilterSamplesByName(focus, nil, nil)
+	rpt := report.NewDefault(c, report.Options{
+		OutputFormat: report.Dot,
+		CumSort:      cum,
+	})
+	data := bytes.NewBuffer(nil)
+	report.Generate(data, rpt, nil)
+	cmd := exec.Command("dot", "-Tsvg")
+	in, _ := cmd.StdinPipe()
+	_, err := io.Copy(in, data)
+	if err != nil {
+		return err
+	}
+	in.Close()
+	out, err := cmd.Output()
+	_, err = w.Write(out)
+	return err
+}
